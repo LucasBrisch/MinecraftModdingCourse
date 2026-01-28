72a775f6827a85793ed6ca1187e7ceb9e6fe8e8b
Lucas Brisch <lucasbrisch@gmail.com>
2026-01-28 20:38:32 -0300

    Feat: adding my first custom itens to minecraft!

--- src/main/java/net/lucas/mccourse/MCCourseMod.java
+++ src/main/java/net/lucas/mccourse/MCCourseMod.java
@@ -2,6 +2,7 @@ package net.lucas.mccourse;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.lucas.mccourse.item.ModItens;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,6 +12,6 @@ public class MCCourseMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-		LOGGER.info("Hello Fabric world!");
+		ModItens.registerModItens();
 	}
 }
---
+++ src/main/java/net/lucas/mccourse/item/ModItens.java
@@ -0,0 +1,34 @@
+package net.lucas.mccourse.item;
+
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.lucas.mccourse.MCCourseMod;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class ModItens {
+
+    public static final Item PINK_GARNET = registerItem("pink_garnet",
+            new Item(new Item.Settings())
+    );
+
+    public static final Item RAW_PINK_GARNET = registerItem("raw_pink_garnet",
+            new Item(new Item.Settings())
+    );
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, Identifier.of(MCCourseMod.MOD_ID, name), item);
+    };
+
+    public static void registerModItens () {
+        MCCourseMod.LOGGER.info("Registering Mod Itens for" + MCCourseMod.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
+            entries.add(PINK_GARNET);
+            entries.add(RAW_PINK_GARNET);
+        });
+    }
+}
